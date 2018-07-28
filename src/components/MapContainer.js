eb3cc7170c0f81f08f348bb0e7068cadc6395fe3
Jesse David <16406332+jesselizdavid@users.noreply.github.com>
2018-07-28 13:19:40 -0400

    STORY-ONE: Add User location to State (#7)
    * STORY-ONE: get user location and set state

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -4,11 +4,34 @@ import { GoogleApiWrapper, Map } from 'google-maps-react';
 const { REACT_APP_GOOGLE_MAPS_API_KEY } = process.env;
 
 export class MapContainer extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      userLat: 0,
+      userLng: 0
+    }
+  }
+
+  componentDidMount() {
+
+   const success = (pos) => {
+     this.setState({userLat: pos.coords.latitude, userLng: pos.coords.longitude});
+   }
+   
+    navigator.geolocation.getCurrentPosition(success)
+  }
+  
   render() {
     return ( 
       <Map 
         google = {this.props.google}
         zoom = {10}
+        center = {
+          {
+            lat: this.state.userLat,
+            lng: this.state.userLng
+          }
+        }
       >
       </Map> 
     );
