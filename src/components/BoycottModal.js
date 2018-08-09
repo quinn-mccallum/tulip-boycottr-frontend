fcca689a5240be28081ee3e2d94c2ad58e0faa67
Valerie MacDougall <33582117+valmacd@users.noreply.github.com>
2018-08-09 18:52:57 -0400

    setting up updatingBoycotts endpoint and connecting the form action to redux (#18)

--- src/components/BoycottModal.js
+++ src/components/BoycottModal.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import BoycottReasonForm from './BoycottForm';
+import { connect } from 'react-redux';
+import { updateBoycotts } from '../actions/boycottActions';
 
-export default ({isActive, onModalToggle}) => (
-        isActive ?
-            <div className={isActive ? 'is-active modal' : 'modal'} >
-                <div className="modal-background"></div>
-                <div className="modal-card">
-                    <header className="modal-card-head">
-                        <button 
-                            className="delete"
-                            aria-label="close"
-                            onClick={() => onModalToggle(!isActive)}
-                        ></button>
-                    </header>
-                    <section className="modal-card-body">
-                        <BoycottReasonForm onSubmit={this.submit} />
-                    </section>
-                    <footer className="modal-card-foot">
-                    </footer>
-                </div>
+const mapStateToProps = (state) => {
+    return {
+      markerData: state.boycottLocations,
+      
+    }
+  
+}
+
+const mapDispatchToProps = {
+      updateBoycotts,
+}
+
+const submit = values => {
+    console.log(this.props.updateBoycotts);
+    //print the form values to console
+    this.props.updateBoycotts(values);
+    
+    //create a post to send the form to the backend
+
+}
+
+const boycottModal = ({isActive, onModalToggle}) => (
+    isActive ?
+        <div className={isActive ? 'is-active modal' : 'modal'} >
+            <div className="modal-background"></div>
+            <div className="modal-card">
+                <header className="modal-card-head">
+                    <button 
+                        className="delete"
+                        aria-label="close"
+                        onClick={() => onModalToggle(!isActive)}
+                    ></button>
+                </header>
+                <section className="modal-card-body">
+                    <BoycottReasonForm onSubmit={submit} />
+                </section>
+                <footer className="modal-card-foot">
+                </footer>
             </div>
-        :
-        null
-    );
+        </div>
+    :
+    null
+);
+
+        
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(boycottModal);
